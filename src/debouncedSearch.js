30c9dd90230ce35ff3a1d31b7925765d78e670ab
ajaybandi14 <ajaybandi14@gmail.com>
2025-02-10 14:00:12 +0530

    render

--- src/debouncedSearch.js
+++ src/debouncedSearch.js
@@ -3,25 +3,43 @@ import './style.css';
 
 export default function DebouncedSearch() {
   const [inputString, setInputString] = useState('');
+  const [result, setResult] = useState([]);
   const debouncedInput = useRef();
+
+  const handleChange = (e) => {
+    if (!e.target.value) {
+      setResult([]);
+    }
+    setInputString(e.target.value);
+  };
 
   useEffect(() => {
     const debouncedSearch = setTimeout(async () => {
-      const fetchResult = await fetch(`https://dummyjson.com/todos?limit=10&skip=80&term=${inputString}`);
-      const todos = await fetchResult.json();
-      console.log('Todos', todos);
+      const fetchResult = await fetch(
+        `https://dummyjson.com/todos?limit=10&skip=80&term=${inputString}`
+      );
+      const res = await fetchResult.json();
+      setResult(res.todos);
     }, 500);
 
-    return (() => {
+    return () => {
       clearTimeout(debouncedSearch);
-    });
+    };
   }, [inputString]);
 
   console.log('Input', inputString);
 
   return (
     <div>
-      <input ref={debouncedInput} type="search" onChange={(e) => setInputString(e.target.value)}/>
+      <input ref={debouncedInput} type="search" onChange={handleChange} />
+      <div>
+        {result.map((r) => (
+          <div>
+            <div>{r.id}</div>
+            <div>{r.todo}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
